dc0a4e47e93857e2021f5b068dbe8faf61cc6e66
jaylate <178095084+jaylate@users.noreply.github.com>
2026-03-13 16:08:30 +0100

    GCP: Use variables and outputs in first tutorial deployment

--- gcp/first-terraform-tutorial/main.tf
+++ gcp/first-terraform-tutorial/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "google" {
-  project = "general-experimentation-490113"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
